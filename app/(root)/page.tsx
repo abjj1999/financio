e05a2c7b3c7c6aad06a35f15f9629a2e76b1a0ff
abdullah al hilfi <abdullahalhilfi21@gmail.com>
2024-05-23 12:02:28 -0500

    home page is kinda working and I just woke up

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,30 @@
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox'
 import React from 'react'
 
 const Home = () => {
+  const loggedIn = {
+    firstName: 'AB',
+  }
   return (
-    <div className=''>
-      
-    </div>
+    <section className='home'>
+      <div className="home-content">
+        <header className="home-header">
+          {/* Welcome AB */}
+          <HeaderBox
+          type="greeting"
+          title="Welcome"
+          user={loggedIn?.firstName || "Guest"}
+          subtext="Access and manage your account."
+          />
+        </header>
+        <TotalBalanceBox
+          accounts = {[]}
+          totalBanks = {1}
+          totalCurrentBalance = {1250.34}
+        />
+      </div>
+    </section>
   )
 }
 
