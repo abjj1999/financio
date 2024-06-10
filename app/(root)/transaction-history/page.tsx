077569af29e44322e572b83327cbfe2a9048279d
abdullah al hilfi <abdullahalhilfi21@gmail.com>
2024-06-09 19:53:31 -0500

    final commit

--- app/(root)/transaction-history/page.tsx
+++ app/(root)/transaction-history/page.tsx
@@ -1,22 +1,30 @@
 import HeaderBox from '@/components/HeaderBox'
+import { Pagination } from '@/components/Pagination';
 import TransactionsTable from '@/components/TransactionsTable';
 import { getAccount, getAccounts } from '@/lib/actions/bank.actions';
 import { getLoggedInUser } from '@/lib/actions/user.actions';
 import { formatAmount } from '@/lib/utils';
 import React from 'react'
 
-const TransactionHistory = async ({searchParams: {id, page}}: SearchParamProps) => {
+const TransactionHistory = async ({ searchParams: { id, page } }: SearchParamProps) => {
 
-  const currentPage = Number(page as string)  || 1;
-  const loggedIn =  await getLoggedInUser()
+  const currentPage = Number(page as string) || 1;
+  const loggedIn = await getLoggedInUser()
   // console.log(loggedIn)
-  const accounts = await getAccounts({userId: loggedIn.$id})
+  const accounts = await getAccounts({ userId: loggedIn.$id })
 
-  if(!accounts) return;
+  if (!accounts) return;
   const accountsData = accounts?.data;
   const appwriteItemId = (id as string) || accountsData[0]?.appwriteItemId;
 
-  const account = await getAccount({appwriteItemId})
+  const account = await getAccount({ appwriteItemId })
+
+  const rowsPerPage = 10
+  const totalPages = Math.ceil(account?.transactions.length / rowsPerPage);
+  const indexOfLastTransaction = currentPage * rowsPerPage;
+  const indexOfFirstTransaction = indexOfLastTransaction - rowsPerPage;
+
+  const currentTransactions = account?.transactions.slice(indexOfFirstTransaction, indexOfLastTransaction);
 
   return (
     <section className='transactions'>
@@ -48,8 +56,14 @@ const TransactionHistory = async ({searchParams: {id, page}}: SearchParamProps) 
           </div>
         </div>
         <section className="flex w-full flex-col gap-6">
-          <TransactionsTable transactions={account?.transactions} />
+          <TransactionsTable transactions={currentTransactions} />
         </section>
+        {totalPages > 1 && (
+          <div className="my-4 w-full">
+            <Pagination totalPages={totalPages} page={currentPage} />
+          </div>
+        )
+        }
       </div>
     </section >
   )
--- components/RightSidebar.tsx
+++ components/RightSidebar.tsx
@@ -3,8 +3,12 @@ import Link from "next/link"
 import { buttonVariants } from "./ui/button"
 import BankCard from "./BankCard"
 import Image from "next/image"
+import Category from "./Category"
+import { countTransactionCategories } from "@/lib/utils"
 
 const RightSidebar = ({user, transactions, banks}: RightSidebarProps) => {
+
+  const categories: CategoryCount[] = countTransactionCategories(transactions)
   return (
     <aside className="right-sidebar">
       <section className="flex flex-col pb-8">
@@ -67,11 +71,11 @@ const RightSidebar = ({user, transactions, banks}: RightSidebarProps) => {
         <div className="mt-10 flex flex-1 flex-col gap-6">
           <h2 className="header-2">Top categories</h2>
 
-          {/* <div className='space-y-5'>
+          <div className='space-y-5'>
             {categories.map((category, index) => (
               <Category key={category.name} category={category} />
             ))}
-          </div> */}
+          </div>
         </div>
       </section>
     </aside>
